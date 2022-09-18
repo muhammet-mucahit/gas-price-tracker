76746d154f3c961fd492f4562af95af3cdbd7351
Mucahit Aktepe <mucahit.aktepe@regiohelden.de>
2022-09-19 02:14:09 +0300

    Fix api endpoint

--- src/services/gas/index.js
+++ src/services/gas/index.js
@@ -5,7 +5,7 @@ import api from "services/api";
 
 export function getGasData() {
   api
-    .request({ url: "/api/gas_price_eip_1559", method: "GET" })
+    .request({ url: "/api/gas_price", method: "GET" })
     .then(({ data }) => store.dispatch(setGasData(data)))
     .catch((error) => store.dispatch(setError(error)));
 }
